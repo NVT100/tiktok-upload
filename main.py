5a7b3547f16c867358a3a8918cef40be2bd8ba24
NVT100 <nvt96753@gmail.com>
2023-10-18 20:15:27 -0500

    a

---
+++ main.py
@@ -0,0 +1,23 @@
+from tiktok_uploader.auth import AuthBackend
+from tiktok_uploader.upload import upload_videos
+number_of_posts_times_6 = 1
+x = 0
+videos = [
+    {
+        'path': 'C:\\Users\\nvt96\\Documents\\Uploader\\a.mp4',
+        'description': 'this is my description'
+    },
+    {
+        'path': 'C:\\Users\\nvt96\\Documents\\Uploader\\a.mp4',
+        'description': 'this is also my description'
+    },
+    {
+        'path': 'C:\\Users\\nvt96\\Documents\\Uploader\\a.mp4',
+        'description': 'this is my description'
+    },
+
+]
+while x <= number_of_posts_times_6:
+    auth = AuthBackend(cookies='C:\\Users\\nvt96\\Documents\\Uploader\\cookies.txt')
+    upload_videos(videos=videos, auth=auth, headless=True)
+    x = x+1
